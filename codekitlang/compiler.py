1e13b8f6e564e2ae4872836cbefaf447ea4c64cb
OCHIAI, Gouji <gjo.ext@gmail.com>
2013-08-22 10:42:43 +0900

    Add parse_string implementation.

--- codekitlang/compiler.py
+++ codekitlang/compiler.py
@@ -1,1 +1,108 @@
 # -*- coding: utf-8 -*-
+
+import logging
+import os
+import re
+
+
+logger = logging.getLogger(__name__)
+special_comment_re = re.compile(
+    r'(?P<wrapper><!--\s*(?:'
+    r'@(?:import|include)\s+(?P<filenames>.*?)'
+    r'|'
+    r'[@$](?P<variable>[a-zA-Z][^\s:=]*)\s*(?:[\s:=]\s*(?P<value>.*?))?'
+    r')-->)',
+    re.DOTALL | re.IGNORECASE | re.LOCALE | re.MULTILINE | re.UNICODE
+)
+
+
+class CompileError(Exception):
+    pass
+
+
+class FileNotFoundError(CompileError):
+    pass
+
+
+class UnknownEncodingError(CompileError):
+    pass
+
+
+class VariableNotFoundError(CompileError):
+    pass
+
+
+def parse_string(s):
+    """
+    @type s: str (Python2.X unicode)
+    @rtype: [(str, str), ...]
+    """
+    parsed = []
+    pos = 0
+    for m in special_comment_re.finditer(s):
+        if m.start('wrapper') > pos:
+            parsed.append(('NOOP', s[pos:m.start('wrapper')]))
+        if m.group('filenames'):
+            for filename in m.group('filenames').split(','):
+                filename = filename.strip().strip('\'"')
+                parsed.append(('JUMP', filename))
+        elif m.group('value'):
+            value = m.group('value').strip()
+            parsed.append(('STOR', (m.group('variable'), value)))
+        else:  # m.group('variable')
+            parsed.append(('LOAD', m.group('variable')))
+        pos = m.end('wrapper')
+    parsed.append(('NOOP', s[pos:]))
+    return parsed
+
+
+def get_filepath(filename, base_path, framework_paths):
+    """
+    @type filename: str
+    @type base_path: str
+    @type framework_paths: [str, ...]
+    @rtype: str
+    """
+    _, ext = os.path.splitext(filename)
+    if not ext:
+        filename += '.kit'
+        ext = '.kit'
+    for prefix in ('', '_'):
+        filepath = os.path.abspath(os.path.join(base_path, prefix + filename))
+        if os.path.exists(filepath):
+            logger.debug('Using %s for %s', filepath, filename)
+            return filepath
+    for prefix in ('', '_'):
+        for path in framework_paths:
+            filepath = os.path.abspath(os.path.join(path, prefix + filename))
+            if os.path.exists(filepath):
+                logger.debug('Using %s for %s', filepath, filename)
+                return filepath
+    return None
+
+
+def bytes_to_str(b, encoding_hints=None):
+    """
+    @type b: bytes (Python2.X str)
+    @type encoding_hints: [str, ...]
+    @rtype: (str, [(str, str), ...])
+    """
+    # TODO: not implemented now
+    return 'utf-8', unicode(b, encoding='utf-8', errors='strict')
+
+
+def get_file_content(filepath, encoding_hints=None):
+    """
+    @type filepath: str
+    @type encoding_hints: [str, ...]
+    @rtype: str
+    """
+    with open(filepath, 'rb') as fp:
+        b = fp.readall()
+    return bytes_to_str(b, encoding_hints)
+
+
+def parse_file(filename, base_path, framework_paths, encoding_hints=None):
+    filepath = get_filepath(filename, base_path, framework_paths)
+    encoding, s = get_file_content(filepath, encoding_hints)
+    return filepath, encoding, parse_string(s)
--- codekitlang/tests/test_compiler.py
+++ codekitlang/tests/test_compiler.py
@@ -1,1 +1,96 @@
 # -*- coding: utf-8 -*-
+
+import unittest
+
+
+class ParseStringTestCase(unittest.TestCase):
+
+    def setUp(self):
+        from ..compiler import parse_string
+        self.func = parse_string
+
+    def test_noop_1(self):
+        ret = self.func('')
+        self.assertEqual([('NOOP', '')], ret)
+
+    def test_noop_2(self):
+        ret = self.func('not comments')
+        self.assertEqual([('NOOP', 'not comments')], ret)
+
+    def test_noop_3(self):
+        ret = self.func('not <!-- dummy comment --> symbol')
+        self.assertEqual([('NOOP', 'not <!-- dummy comment --> symbol')], ret)
+
+    def test_jump_1(self):
+        ret = self.func('pre <!--@include aaa--> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('JUMP', 'aaa'),
+                          ('NOOP', ' post')], ret)
+
+    def test_jump_2(self):
+        ret = self.func('pre <!-- @import "b b b" --> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('JUMP', 'b b b'),
+                          ('NOOP', ' post')], ret)
+
+    def test_jump_3(self):
+        ret = self.func("pre <!-- @INCLUDE ccc, 'd d d' --> post")
+        self.assertEqual([('NOOP', 'pre '),
+                          ('JUMP', 'ccc'),
+                          ('JUMP', 'd d d'),
+                          ('NOOP', ' post')], ret)
+
+    def test_jump_4(self):
+        ret = self.func("""pre <!--
+        @INCLUDE
+        eee,
+        fff
+        --> post""")
+        self.assertEqual([('NOOP', 'pre '),
+                          ('JUMP', 'eee'),
+                          ('JUMP', 'fff'),
+                          ('NOOP', ' post')], ret)
+
+    def test_load_1(self):
+        ret = self.func('pre <!--@var1--> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('LOAD', 'var1'),
+                          ('NOOP', ' post')], ret)
+
+    def test_load_2(self):
+        ret = self.func('pre <!-- $VAR2 --> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('LOAD', 'VAR2'),
+                          ('NOOP', ' post')], ret)
+
+    def test_stor_1(self):
+        ret = self.func('pre <!--@var_A val1--> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('STOR', ('var_A', 'val1')),
+                          ('NOOP', ' post')], ret)
+
+    def test_stor_2(self):
+        ret = self.func('pre <!-- @var_B:val2 --> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('STOR', ('var_B', 'val2')),
+                          ('NOOP', ' post')], ret)
+
+    def test_stor_3(self):
+        ret = self.func('pre <!-- $var_C = v a l 3 --> post')
+        self.assertEqual([('NOOP', 'pre '),
+                          ('STOR', ('var_C', 'v a l 3')),
+                          ('NOOP', ' post')], ret)
+
+    def test_stor_4(self):
+        ret = self.func("""pre <!--
+        $var_D
+very
+long
+value
+        --> post""")
+        self.assertEqual([('NOOP', 'pre '),
+                          ('STOR', ('var_D', 'very\nlong\nvalue')),
+                          ('NOOP', ' post')], ret)
+
+    def tearDown(self):
+        pass
