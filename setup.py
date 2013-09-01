ebfd9feddfcbc3b0d4de96ffc882c40b48684646
OCHIAI, Gouji <gjo.ext@gmail.com>
2013-09-01 12:32:52 +0900

    Fix version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CodeKitLang',
-    version='0.1dev',
+    version='0.1',
     description='CodeKit Language Compiler, Python implementation',
     long_description='\n\n'.join([
         open('README.rst').read(),
