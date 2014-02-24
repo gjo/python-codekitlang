dc11db5a96bf5448870f80b4b8edf2eca9f77f18
OCHIAI, Gouji <gjo.ext@gmail.com>
2014-02-25 01:42:04 +0900

    Go to develop mode.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class PyTest(TestCommand):
 
 setup(
     name='CodeKitLang',
-    version='0.2',
+    version='0.3dev',
     description='CodeKit Language Compiler, Python implementation',
     long_description='\n\n'.join([
         open('README.rst').read(),
