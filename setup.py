2d74565d488380a0168d7fb757d8a60911594bd3
OCHIAI, Gouji <gjo.ext@gmail.com>
2015-03-09 03:11:01 +0900

    Prepare release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class PyTest(TestCommand):
 
 setup(
     name='CodeKitLang',
-    version='0.4dev',
+    version='0.4',
     description='CodeKit Language Compiler, Python implementation',
     long_description='\n\n'.join([
         open('README.rst').read(),
